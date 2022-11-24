5f9a86a4a8a4c1c1db665266d2365c9cf1799eec
Max <maksym.illarionov@nure.ua>
2022-11-25 01:24:58 +0200

    Empty project

---
+++ Module4ModuleTask/Module4ModuleTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module4ModuleTask
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
